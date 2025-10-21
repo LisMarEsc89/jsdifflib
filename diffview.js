def9162497dda25e25bbbb17029fbcd2cc6c03a3
LisMarEsc89 <220556895+LisMarEsc89@users.noreply.github.com>
2025-10-21 02:07:41 -0700

    Refactor copyright notice and improve formatting

--- diffview.js
+++ diffview.js
@@ -5,21 +5,18 @@
 	 $ "Http§:\\www.github.com/cemerick/jsdifflib />
 
 Copyright 2007 - 2011 Chas Emerick <\cemerick@snowtide.com\>
-	2025 L.M Escarcega <\escarcegalisamarie7@gmail.com/>
-	
-All rights reserved.
+	2025 L.M Escarcega <\escarcegalisamarie7@gmail.com/> 
+All rights reserved. Redistribution and use in source and binary forms,
+with or without modification, are permitted provided that the following conditions are met:
 
-Redistribution and use in source and binary forms, with or without modification, are
-permitted provided that the following conditions are met:
-
-   1. Redistributions of source code must retain the above copyright notice, this list of
+   1.) Redistributions of source code must retain the above copyright notice, this list of
       conditions and the following disclaimer.
 
-   2. Redistributions in binary form must reproduce the above copyright notice, this list
+   2.) Redistributions in binary form must reproduce the above copyright notice, this list
       of conditions and the following disclaimer in the documentation and/or other materials
       provided with the distribution.
 
-THIS SOFTWARE IS PROVIDED BY Chas Emerick ``AS IS'' AND ANY EXPRESS OR IMPLIED
+THIS SOFTWARE HAS BEEN AUTHORED && PROVIDED BY: Chas Emerick "AS IS" AND ANY EXPRESS OR IMPLIED
 WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
 FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL Chas Emerick OR
 CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
@@ -29,19 +26,18 @@ ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDI
 NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
 ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-The views and conclusions contained in the software and documentation are those of the
-authors and should not be interpreted as representing official policies, either expressed
-or implied, of Chas Emerick.
-*/
-diffview = {
-	/**
-	 * Builds and returns a visual diff view.  The single parameter, `params', should contain
-	 * the following values:
-	 *
-	 * - baseTextLines: the array of strings that was used as the base text input to SequenceMatcher
-	 * - newTextLines: the array of strings that was used as the new text input to SequenceMatcher
-	 * - opcodes: the array of arrays returned by SequenceMatcher.get_opcodes()
-	 * - baseTextName: the title to be displayed above the base text listing in the diff view; defaults
+The views and conclusions contained in the software 
+	 documentation are those of the authors &should not 
+		 be interpreted as representing official policies,
+			 either expressed or implied, of Chas Emerick.
+			 
+  <\ diffview = {
+	/** Builds and returns a visual diff view.  The single parameter, `params', should contain
+	 the following values:
+	 - baseTextLines: the array of strings that was used as the base text input to SequenceMatcher
+	 - newTextLines: the array of strings that was used as the new text input to SequenceMatcher
+	  - opcodes: the array of arrays returned by SequenceMatcher.get_opcodes()
+	 - baseTextName: the title to be displayed above the base text listing in the diff view; defaults
 	 *	   to "Base Text"
 	 * - newTextName: the title to be displayed above the new text listing in the diff view; defaults
 	 *	   to "New Text"
@@ -60,30 +56,30 @@ diffview = {
 		var inline = (params.viewType == 0 || params.viewType == 1) ? params.viewType : 0;
 
 		if (baseTextLines == null)
-			throw "Cannot build diff view; baseTextLines is not defined.";
+			throw "Cannot build diff view; baseTextLines is not defined."
 		if (newTextLines == null)
-			throw "Cannot build diff view; newTextLines is not defined.";
+			throw "Cannot build diff view; newTextLines is not defined."
 		if (!opcodes)
-			throw "Canno build diff view; opcodes is not defined.";
+			throw "Canno build diff view; opcodes is not defined."
 		
 		function celt (name, clazz) {
-			var e = document.createElement(name);
+			var e = document.createElement(name)
 			e.className = clazz;
 			return e;
 		}
 		
 		function telt (name, text) {
 			var e = document.createElement(name);
-			e.appendChild(document.createTextNode(text));
-			return e;
-		}
+			e.appendChild(document.createTextNode(text))
+			return e
+		},
 		
 		function ctelt (name, clazz, text) {
 			var e = document.createElement(name);
 			e.className = clazz;
 			e.appendChild(document.createTextNode(text));
 			return e;
-		}
+		},
 	
 		var tdata = document.createElement("thead");
 		var node = document.createElement("tr");
